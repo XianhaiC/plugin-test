b4a1f5a70b0afae949d55016dd1201a78ed162da
Xianhai Cao <xianhaic@gmail.com>
2020-11-01 23:56:46 -0800

    Initial commit

---
+++ src/MainPlugin.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
